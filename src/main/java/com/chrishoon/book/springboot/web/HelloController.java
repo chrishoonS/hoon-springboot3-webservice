b94c165cfcf2622a44f397e54ebb130bb10052fe
송지훈 <jihoon.song@medisolution.co.kr>
2025-01-20 15:11:28 +0900

    JSON 리턴 API 테스트

--- src/main/java/com/chrishoon/book/springboot/web/HelloController.java
+++ src/main/java/com/chrishoon/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.chrishoon.book.springboot.web;
 
+import com.chrishoon.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 컨트롤러를 JSON으로 반환하는 컨트롤러로 생성
@@ -13,4 +15,13 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(
+            @RequestParam("name") String name,  // 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+            @RequestParam("amount") int amount
+    ) {
+        return new HelloResponseDto(name, amount);
+    }
+
 }
--- src/test/java/com/chrishoon/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/chrishoon/book/springboot/web/HelloControllerTest.java
@@ -1,25 +1,26 @@
 package com.chrishoon.book.springboot.web;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
-//import org.springframework.test.web.servlet.MockMvc;
+import org.junit.runner.RunWith;
 
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 // Junit4 이용하는 경우
-/*@RunWith(SpringRunner.class)        // SpringRunner 스프링 실행자 사용
+@RunWith(SpringRunner.class)        // SpringRunner 스프링 실행자 사용
 @WebMvcTest(HelloController.class)  // 컨트롤러 관련 어노테이션 사용가능
 class HelloControllerTest {
 
@@ -28,13 +29,13 @@ class HelloControllerTest {
 
     // Mockito is currently self-attaching to enable the inline-mock-maker. This will no longer work in future releases of the JDK. Please add Mockito as an agent to your build what is described in Mockito's documentation:
     // 프로젝트의 JDK 버전과 Mockito 설정 간의 호환성에 대한 사전 알림
-    *//*
-        해결책
-        - mockito-inline 의존성을 추가
-        - JDK 모듈 접근을 허용하는 JVM 옵션을 사용하는 것도 가능
-          JVM 옵션 : --add-opens java.base/java.lang=ALL-UNNAMED
-        - 최신 Mockito 버전을 사용하여 최신 변경사항을 적용
-     *//*
+
+    // 해결책
+    // mockito-inline 의존성을 추가
+    // JDK 모듈 접근을 허용하는 JVM 옵션을 사용하는 것도 가능
+    // JVM 옵션 : --add-opens java.base/java.lang=ALL-UNNAMED
+    // 최신 Mockito 버전을 사용하여 최신 변경사항을 적용
+
     @Test
     public void hello_리턴() throws Exception {
         String hello = "hello";
@@ -44,10 +45,24 @@ class HelloControllerTest {
                 .andExpect(content().string(hello));    // 응답 본문의 내용 검증
 
     }
-}*/
+
+    @Test
+    public void helloDto_리턴() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)                        // 요청 파라미터 설정, only String
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))        // JSON 응답값을 필드별로 검증, $를 기준으로 필드명 명시
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+    }
+}
 
 //Junit5 이용하는 경우
-@WebFluxTest(HelloController.class) // WebFlux와 관련된 테스트를 설정
+/*@WebFluxTest(HelloController.class) // WebFlux와 관련된 테스트를 설정
 class HelloControllerTest {
 
     @Autowired
@@ -58,10 +73,28 @@ class HelloControllerTest {
     public void returnHello() {
         String hello = "hello";
 
-        webTestClient.get() // HTTP GET 요청
-                .uri("/hello") // URI 지정
-                .exchange() // 요청 실행
-                .expectStatus().isOk() // HTTP 상태 코드 검증
+        webTestClient.get()                                 // HTTP GET 요청
+                .uri("/hello")                          // URI 지정
+                .exchange()                                 // 요청 실행
+                .expectStatus().isOk()                      // HTTP 상태 코드 검증
                 .expectBody(String.class).isEqualTo(hello); // 응답 본문 내용 검증
     }
-}
+
+    @Test
+    @DisplayName("helloDto 리턴 테스트")
+    public void returnHelloDto() {
+        String name = "hello";
+        int amount = 1000;
+
+        webTestClient.get().uri(uriBuilder -> uriBuilder.path("/hello/dto")
+                        .queryParam("name", name)
+                        .queryParam("amount", amount)
+                        .build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()                                         // 응답 본문 검증
+                .jsonPath("$.name").isEqualTo(name)         // JSON 경로 "name" 필드 확인
+                .jsonPath("$.amount").isEqualTo(amount);    // JSON 경로 "amount" 필드 확인
+
+    }
+}*/
