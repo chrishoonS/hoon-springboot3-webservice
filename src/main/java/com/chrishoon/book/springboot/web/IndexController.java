60d35d79814f3eba4f9d2029fa96b107749fb28f
송지훈 <jihoon.song@medisolution.co.kr>
2025-01-21 17:39:13 +0900

    mustache 템플릿 엔진 사용을 위한 기본 세팅

---
+++ src/main/java/com/chrishoon/book/springboot/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.chrishoon.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // 머스테치 스타터가 컨트롤러에서 문자열 반환 시 앞의 경로와 뒤의 파일 확장자는 자동으로 지정
+    }
+
+}
